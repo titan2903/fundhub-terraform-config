11c453ed4b938e41022aea36b121bf7e474307a0
titan <titanioy98@gmail.com>
2023-12-17 12:54:15 +0700

    debug

--- terragrunt/staging/instances/instance-mariadb/.terragrunt-cache/RbGcW12zY4wj0OgtcusXX11YWE4/FnLgdxR11APghBDJz7j2-44JeJg/output.tf
+++
@@ -1,7 +0,0 @@
-output "private_ip" {
-  value = google_compute_instance.default.network_interface.0.network_ip
-}
-
-output "public_ip" {
-  value = google_compute_instance.default.network_interface.0.access_config.0.nat_ip
-}
